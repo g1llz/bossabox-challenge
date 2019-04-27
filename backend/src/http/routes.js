3642766a73f8ecbf9bab75c5a8f0b5763b2ca1c0
Gilles Lima <gillestoim@gmail.com>
2019-04-27 12:19:21 -0300

    add new routes

--- backend/src/http/routes.js
+++ backend/src/http/routes.js
@@ -1,11 +1,34 @@
-const URI = '/api/v1';
+const model = require('../models');
 
 const routes = (server) => {
-  server.get(`${URI}/tools`, async (req, res, next) => {
+  server.get(`${process.env.URI}/tools`, async (req, res, next) => {
     try {
-      res.send('test...');
+      const tools = await model.tool().find();
+      res.json(tools);
     } catch (error) {
-      res.send(error);
+      res.json(error);
+    }
+    next();
+  });
+
+  server.get(`${process.env.URI}/tools/:id`, async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const tool = await model.tool().findById(id);
+      res.json(tool);
+    } catch (error) {
+      res.json(error);
+    }
+    next();
+  });
+
+  server.post(`${process.env.URI}/tools`, async (req, res, next) => {
+    const { ...data } = req.body;
+    try {
+      const tools = await model.tool().create(data);
+      res.json(tools);
+    } catch (error) {
+      res.json(error);
     }
     next();
   });
