0e43ce3fadd5edfcbd229d28dfd933885f62c50a
Gilles Lima <gillestoim@gmail.com>
2019-07-06 22:10:46 -0300

    update routes

--- backend/src/http/routes.js
+++ backend/src/http/routes.js
@@ -1,17 +1,22 @@
 const toolService = require('../services/Tool');
 const userService = require('../services/User');
+const authService = require('../services/Auth');
 
 const routes = (server) => {
   server.get(`${process.env.URI}/tools`, toolService.list);
   server.get(`${process.env.URI}/tools/:id`, toolService.listById);
   server.post(`${process.env.URI}/tools`, toolService.create);
-  server.patch(`${process.env.URI}/tools/:id`, toolService.update);
+  server.put(`${process.env.URI}/tools/:id`, toolService.update);
   server.del(`${process.env.URI}/tools/:id`, toolService.delete);
 
+  // TODO: document this part;
   server.get(`${process.env.URI}/users`, userService.list);
   server.get(`${process.env.URI}/users/:id`, userService.listById);
-  server.post(`${process.env.URI}/users`, userService.create);
   server.del(`${process.env.URI}/users/:id`, userService.delete);
+
+  // TODO: implement role check; document this part;
+  server.post(`${process.env.URI}/auth`, authService.login);
+  server.post(`${process.env.URI}/register`, userService.create);
 }
 
 module.exports = routes;
