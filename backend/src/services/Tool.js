acf8681826b9c75649cb470b7c864a258deb0813
Gilles Lima <gillestoim@gmail.com>
2019-07-07 13:04:10 -0300

    now user manage only own tools collection

--- backend/src/services/Tool.js
+++ backend/src/services/Tool.js
@@ -6,14 +6,11 @@ module.exports = {
     let data = {};
     try {
       if (tag) data = { tags: tag };
-      const tools = await Tool.find(data);
+      const tools = await Tool.find({ owner: req.decoded.id, ...data });
 
-      if (!tools.length) {
+      if (tag && !tools.length) {
         res.status(404);
-        res.json({
-          status: 404,
-          message: 'No tool found',
-        });
+        res.json({ status: 404, message: 'No tool found' });
         return;
       }
       res.json(tools);
@@ -25,13 +22,10 @@ module.exports = {
   listById: async (req, res, next) => {
     const { id } = req.params;
     try {
-      const tool = await Tool.findById(id);
+      const tool = await Tool.findOne({ owner: req.decoded.id, _id: id });
       if (!tool) {
         res.status(404);
-        res.json({
-          status: 404,
-          message: 'No tool found by id',
-        });
+        res.json({ status: 404, message: 'No tool found by id' });
         return;
       }
       res.json(tool);
@@ -42,8 +36,10 @@ module.exports = {
   },
   create: async (req, res, next) => {
     const { ...data } = req.body;
+    // 'owner' can't be comming in the request body; check and remove;
+    if ('owner' in data) delete data.owner;
     try {
-      const tool = await Tool.create(data);
+      const tool = await Tool.create({ ...data, owner: req.decoded.id });
       res.status(201);
       res.json(tool);
     } catch (error) {
@@ -52,10 +48,7 @@ module.exports = {
           { field: key, text: error.errors[key].message }
         ));
         res.status(400);
-        res.json({
-          status: 400,
-          errors: messages,
-        });
+        res.json({ status: 400, errors: messages });
         return;
       }
       res.json(error)
@@ -66,12 +59,9 @@ module.exports = {
     const { id } = req.params;
     const { ...data } = req.body;
     try {
-      await Tool.findByIdAndUpdate(id, data);
+      await Tool.findOneAndUpdate({ owner: req.decoded.id, _id: id }, data);
       res.status(200);
-      res.json({
-        status: 200,
-        message: 'Tool updated successfully',
-      });
+      res.json({ status: 200, message: 'Tool updated successfully' });
     } catch (error) {
       res.json(error);
     }
@@ -80,15 +70,12 @@ module.exports = {
   delete: async (req, res, next) => {
     const { id } = req.params;
     try {
-      await Tool.findByIdAndDelete(id);
+      await Tool.findOneAndDelete({ owner: req.decoded.id, _id: id });
       res.json({});
     } catch (error) {
       if (error.name === 'CastError') {
         res.status(400);
-        res.json({
-          status: 400,
-          error: { kind: error.kind, message: error.message },
-        });
+        res.json({ status: 400, error: { kind: error.kind, message: error.message } });
         return;
       }
       res.json(error);
